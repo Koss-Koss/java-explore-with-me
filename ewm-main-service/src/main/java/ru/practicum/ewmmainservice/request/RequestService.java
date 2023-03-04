6f4f087ecf37d55a8821f328670ccccc64f6c850
Константин Михалев <mikhalevks@yandex.ru>
2023-03-04 17:21:35 +0300

    refactor: стиль кода после check style

--- ewm-main-service/src/main/java/ru/practicum/ewmmainservice/request/RequestService.java
+++ ewm-main-service/src/main/java/ru/practicum/ewmmainservice/request/RequestService.java
@@ -7,11 +7,16 @@ import ru.practicum.ewmmainservice.request.model.dto.ParticipationRequestDto;
 import java.util.List;
 
 public interface RequestService {
+
     ParticipationRequestDto create(long userId, long eventId);
+
     ParticipationRequestDto cancel(long userId, long requestId);
+
     EventRequestStatusUpdateResultDto updateRequestStatus(
             long userId, long eventId, EventRequestStatusUpdateDto eventRequestStatusUpdateDto);
+
     List<ParticipationRequestDto> findAllByEventIdAndInitiatorId(long userId, long eventId);
+    
     List<ParticipationRequestDto> findAllByRequesterId(long userId);
 
 }
